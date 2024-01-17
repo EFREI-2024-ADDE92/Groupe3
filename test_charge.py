062db89e52912e2fd02e6bf15d61287269296931
SarankanSivananthan <sarankan.sivananthan@efrei.net>
2024-01-17 21:50:41 +0100

    test_charge scripts

---
+++ test_charge.py
@@ -0,0 +1,9 @@
+from locust import HttpUser, task, between
+
+class MyUser(HttpUser):
+    wait_time = between(1, 5)  # Wait time between requests
+
+    @task
+    def predict_endpoint(self):
+        # Define your request to the prediction endpoint
+        self.client.post("https://group3-container.graycoast-fbecffe8.francecentral.azurecontainerapps.io/predict", json={"features": [5.1, 3.5,1.4, 0.2]})

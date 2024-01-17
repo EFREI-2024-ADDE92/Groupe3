0f440b5002fb0195e2622fbbf1166480a40a530a
Ulysse B <ulysse.bonneau@proton.me>
2024-01-17 22:19:43 +0100

    more vus

--- test_charge.js
+++ test_charge.js
@@ -2,25 +2,20 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  vus: 10,  // Nombre de VUs (utilisateurs virtuels)
-  duration: '30s',  // Durée totale du test
+  vus: 100,
+  duration: '30s',
 };
 
 export default function () {
-  // Définissez l'URL de votre API
   const url = 'https://group3-container.graycoast-fbecffe8.francecentral.azurecontainerapps.io/predict';
 
-  // Définissez les données JSON pour la requête POST
-  const payload = {"features": [5.1, 3.5,1.4, 0.2]}
+  const payload = { "features": [5.1, 3.5, 1.4, 0.2] }
 
-  // Définissez les en-têtes de la requête
   const headers = {
     'Content-Type': 'application/json'
   };
 
-  // Envoyez la requête POST avec les données JSON
   const response = http.post(url, JSON.stringify(payload), { headers: headers });
 
-  // Ajoutez une pause (simulation d'une charge constante)
   sleep(1);
 }
